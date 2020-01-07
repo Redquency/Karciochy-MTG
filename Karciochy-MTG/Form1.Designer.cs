6160baf0008064cbdba68a07b35276974e2f2d1a
Piotr Redmerski <piotr.redmerski@gmail.com>
2020-01-07 13:40:00 +0100

    Add project files.

---
+++ Karciochy-MTG/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Karciochy_MTG
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GetCardsButton = new System.Windows.Forms.Button();
+            this.cardNameTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SetComboBox = new System.Windows.Forms.ComboBox();
+            this.Lp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CardName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Set = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Image = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Lp,
+            this.CardName,
+            this.Set,
+            this.Info,
+            this.Link,
+            this.Image});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(667, 620);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // GetCardsButton
+            // 
+            this.GetCardsButton.Location = new System.Drawing.Point(12, 8);
+            this.GetCardsButton.Name = "GetCardsButton";
+            this.GetCardsButton.Size = new System.Drawing.Size(330, 54);
+            this.GetCardsButton.TabIndex = 2;
+            this.GetCardsButton.Text = "Get Cards";
+            this.GetCardsButton.UseVisualStyleBackColor = true;
+            this.GetCardsButton.Click += new System.EventHandler(this.GetCardsButton_Click);
+            // 
+            // cardNameTextBox
+            // 
+            this.cardNameTextBox.Location = new System.Drawing.Point(6, 20);
+            this.cardNameTextBox.Name = "cardNameTextBox";
+            this.cardNameTextBox.Size = new System.Drawing.Size(94, 20);
+            this.cardNameTextBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cardNameTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(348, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(109, 54);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Card Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.SetComboBox);
+            this.groupBox2.Location = new System.Drawing.Point(463, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(207, 54);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Set";
+            // 
+            // SetComboBox
+            // 
+            this.SetComboBox.FormattingEnabled = true;
+            this.SetComboBox.Location = new System.Drawing.Point(6, 18);
+            this.SetComboBox.Name = "SetComboBox";
+            this.SetComboBox.Size = new System.Drawing.Size(195, 21);
+            this.SetComboBox.TabIndex = 6;
+            // 
+            // Lp
+            // 
+            this.Lp.HeaderText = "Lp";
+            this.Lp.Name = "Lp";
+            this.Lp.ReadOnly = true;
+            // 
+            // CardName
+            // 
+            this.CardName.HeaderText = "Card Name";
+            this.CardName.Name = "CardName";
+            this.CardName.ReadOnly = true;
+            this.CardName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // Set
+            // 
+            this.Set.HeaderText = "Set";
+            this.Set.Name = "Set";
+            this.Set.ReadOnly = true;
+            // 
+            // Info
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Info.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Info.HeaderText = "Info";
+            this.Info.Name = "Info";
+            this.Info.ReadOnly = true;
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.ReadOnly = true;
+            // 
+            // Image
+            // 
+            this.Image.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle2.NullValue")));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.Image.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Image.DividerWidth = 1;
+            this.Image.HeaderText = "Image";
+            this.Image.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Image.MinimumWidth = 100;
+            this.Image.Name = "Image";
+            this.Image.ReadOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 700);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.GetCardsButton);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Karciochy MTG";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button GetCardsButton;
+        private System.Windows.Forms.TextBox cardNameTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox SetComboBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CardName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Set;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Info;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+        private System.Windows.Forms.DataGridViewImageColumn Image;
+    }
+}
+
---
+++ Karciochy-MTG/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MtgApiManager.Lib.Core;
+using MtgApiManager.Lib.Model;
+using MtgApiManager.Lib.Service;
+using System.Threading;
+
+namespace Karciochy_MTG
+{
+    public partial class MainForm : Form
+    {
+        private CardService cardService;
+        private List<List<Card>> cardPages;
+        private string setName;
+        public MainForm()
+        {
+            InitializeComponent();
+
+
+            dataGridView1.RowTemplate.Height = 150;
+
+            cardService = new CardService();
+
+
+            //var sets = setService.
+            cardPages = new List<List<Card>>();
+
+
+
+
+
+            //var ff = GetSet();
+            //GetCardsButton.Enabled = false;
+
+            GetSet();
+
+
+
+
+
+
+        }
+
+        void GetSet()
+        {
+            var setService = new SetService();
+            var s =  setService.All();
+            if (s.IsSuccess)
+            {
+                var exp = s.Value.Select(d=>d.Name).ToList<string>();
+                SetComboBox.Items.AddRange(exp.ToArray<string>());
+                SetComboBox.SelectedItem = SetComboBox.Items[0];
+            }
+            else throw new Exception("FAIL");
+
+        }
+
+        async Task SetupCards(string CardName, string CardSet)
+        {
+            cardPages.Clear();
+            var cardCollection = cardService.Where(z => z.SetName, CardSet).Where(x => x.Rarity, "Common").Where(c => c.Name, CardName);
+            var getCollection = await cardCollection.AllAsync();
+            int pagesCount = getCollection.PagingInfo.TotalPages;
+
+            for (int i = 0; i <= pagesCount; i++)
+            {
+                var page = cardCollection.Where(p => p.Page, i + 1).All();
+                if (page.IsSuccess)
+                {
+                    cardPages.Add(page.Value);
+                }
+
+            }
+            
+        }
+
+        public async Task<MtgApiManager.Lib.Core.Exceptional<Card>> GetCard()
+        {
+            var result = cardService.Find("f2eb06047a3a8e515bff62b55f29468fcde6332a");
+            var asyncResult = await cardService.FindAsync("f2eb06047a3a8e515bff62b55f29468fcde6332a");
+            return asyncResult;
+
+        }
+
+        async public Task<Image> GetImage(string fromUrl)
+        {
+            using (System.Net.WebClient webClient = new System.Net.WebClient())
+            {
+                using (Stream stream = await webClient.OpenReadTaskAsync(fromUrl))
+                {
+                    return System.Drawing.Image.FromStream(stream);
+                }
+            }
+        }
+
+        public System.Drawing.Image DownloadImage(string imageUrl)
+        {
+            System.Drawing.Image image = null;
+
+            try
+            {
+                System.Net.HttpWebRequest webRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+                webRequest.ServicePoint.ConnectionLeaseTimeout = 5000;
+                webRequest.ServicePoint.MaxIdleTime = 5000;
+
+                using (System.Net.WebResponse webResponse = webRequest.GetResponse())
+                {
+
+                    using (System.IO.Stream stream = webResponse.GetResponseStream())
+                    {
+                        image = System.Drawing.Image.FromStream(stream);
+                    }
+                }
+
+                webRequest.ServicePoint.CloseConnectionGroup(webRequest.ConnectionGroupName);
+                webRequest = null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+
+            }
+
+
+            return image;
+        }
+
+        private void GetCardsButton_Click(object sender, EventArgs e)
+        {
+            GetCardsButton.Enabled = false;
+  
+            setName = SetComboBox.Text;
+            dataGridView1.Rows.Clear();
+            Task.Run(SetCards);
+
+
+
+
+        }
+
+        async public Task SetCards()
+        {
+            await SetupCards(cardNameTextBox.Text, setName);
+
+            foreach (var cardPage in cardPages)
+            {
+                 foreach (var card in cardPage)
+                 {
+                     if (card.ImageUrl != null)
+                     {
+                        _ = Task.Run(() =>
+                          {
+                              Image img = DownloadImage(card.ImageUrl.AbsoluteUri);
+
+                              if (dataGridView1.InvokeRequired)
+                              {
+                                  dataGridView1.Invoke(new MethodInvoker(() =>
+                                  {
+                                      dataGridView1.Rows.Add(new object[] { dataGridView1.Rows.Count, card.Name, card.SetName, card.Text, card.ImageUrl, img });
+                                      dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0];
+                                    //dataGridView1.Refresh();
+                                  }));
+
+                              }
+                              else
+                              {
+                                  dataGridView1.Rows.Add(new object[] { dataGridView1.Rows.Count, card.Name, card.SetName, card.Text, card.ImageUrl, img });
+                                  dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0];
+                                //dataGridView1.Refresh();
+                              }
+                          });
+                             
+                         
+
+
+
+                     }
+                 }
+            }
+
+             if (GetCardsButton.InvokeRequired)
+                 GetCardsButton.Invoke(new MethodInvoker(() => {
+                     GetCardsButton.Enabled = true;
+                 }));
+             else GetCardsButton.Enabled = true;
+
+             
+
+
+        }
+
+        //private async Task DownloadImageAsync(dataGridViv Uri uri)
+        //{
+        //    var httpClient = new HttpClient();
+
+        //    // Get the file extension
+        //    var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
+        //    var fileExtension = Path.GetExtension(uriWithoutQuery);
+
+        //    // Create file path and ensure directory exists
+        //    var path = Path.Combine(directoryPath, $"{fileName}{fileExtension}");
+        //    Directory.CreateDirectory(directoryPath);
+
+        //    // Download the image and write to the file
+        //    var imageBytes = await httpClient.GetStreamAsync(uri);
+        //}
+
+
+    }
+}
